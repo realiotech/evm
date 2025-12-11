b2036119a8ec9910900accb7ab582248759a8cd1
Hieu Vu <hieuvubk@gmail.com>
2025-12-11 10:11:44 +0700

    support old tx types

---
+++ x/vm/types/oldtx/msg.go
@@ -0,0 +1,48 @@
+package oldtx
+
+import (
+	"fmt"
+
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+
+	errorsmod "cosmossdk.io/errors"
+
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+var (
+	_ codectypes.UnpackInterfacesMessage = MsgEthereumTx{}
+
+)
+
+// UnpackInterfaces implements UnpackInterfacesMessage.UnpackInterfaces
+func (msg MsgEthereumTx) UnpackInterfaces(unpacker codectypes.AnyUnpacker) error {
+	return unpacker.UnpackAny(msg.Data, new(TxData))
+}
+
+// AsTransaction creates an Ethereum Transaction type from the msg fields
+func (msg MsgEthereumTx) AsTransaction() *ethtypes.Transaction {
+	txData, err := UnpackTxData(msg.Data)
+	fmt.Println("UnpackTxData", err)
+	if err != nil {
+		return nil
+	}
+
+	return ethtypes.NewTx(txData.AsEthereumData())
+}
+
+// UnpackTxData unpacks an Any into a TxData. It returns an error if the
+// client state can't be unpacked into a TxData.
+func UnpackTxData(anyTxData *codectypes.Any) (TxData, error) {
+	if anyTxData == nil {
+		return nil, errorsmod.Wrap(errortypes.ErrUnpackAny, "protobuf Any message cannot be nil")
+	}
+
+	txData, ok := anyTxData.GetCachedValue().(TxData)
+	if !ok {
+		return nil, errorsmod.Wrapf(errortypes.ErrUnpackAny, "cannot unpack Any into TxData %T", anyTxData)
+	}
+
+	return txData, nil
+}
